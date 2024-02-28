3041d96c36f7afbd1f1b696553ddaf5d5e285494
pavankumaranagani <pavankumaranagani47@gamil.com>
2024-02-28 17:23:40 +0530

    first commit

---
+++ password.py
@@ -0,0 +1,20 @@
+import random
+
+print('Welocome To Your Passwords Generator')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$&1234567890'
+
+number = input('Amount Of Password to generate: ')
+
+number = int(number)
+
+length = input('Input Your Password length: ')
+length = int(length)
+
+print('\nhere are your passwords: ')
+
+for pwd in range(number):
+    password = ''
+    for c in range(length):
+      password += random.choice(chars)
+    print(password)    
